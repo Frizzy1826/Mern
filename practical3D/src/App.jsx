4cc352c8c81483f46c92a404a5ae342449a79d4f
Frizzy1826 <Bhavsalamvlu@gmail.com>
2026-07-15 20:33:09 +0530

    Initial commit

---
+++ practical3D/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App(){
+  return (
+    <div className="container">
+      <h1>Student Dashboard</h1>
+      <hr />
+      <p>Name : Bhavesh Salaskar</p>
+      <p>Roll no :39</p>
+      <p>course: BSCIT</p>
+      <p>Semester: V</p>
+      <button>view Result</button>
+    </div>
+  );
+}
+export default App;
