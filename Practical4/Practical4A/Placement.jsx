60979af6138a15b3635f4ad2966c47c8769cd3c7
bhavesh <bhavsalamvlu@gmail.com>
2026-07-19 19:05:56 +0530

    Add Placement component to display placement status

---
+++ Practical4/Practical4A/Placement.jsx
@@ -0,0 +1,11 @@
+function Placements() {
+  return (
+    <section>
+      <h2>Placement Status</h2>
+      <p>Placed: Yes</p>
+      <p>Company: ABC Technologies</p>
+    </section>
+  );
+}
+
+export default Placements;
