af7ad955b2821bbe08f66e0377ff942e985d90e5
bhavesh <bhavsalamvlu@gmail.com>
2026-07-19 19:06:45 +0530

    Add Student component to display student details

---
+++ Practical4/Practical4A/student.jsx
@@ -0,0 +1,11 @@
+function Student() {
+  return (
+    <section>
+      <h2>Student Details</h2>
+      <p>Name: Bhavesh Salaskar</p>
+      <p>Course: BSC IT</p>
+    </section>
+  );
+}
+
+export default Student;
