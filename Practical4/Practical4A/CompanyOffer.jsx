d4ab3ad23f2775f9341aff3a53ce90eaef6ed2dc
bhavesh <bhavsalamvlu@gmail.com>
2026-07-19 19:04:24 +0530

    Add CompanyOffer component with offer details

---
+++ Practical4/Practical4A/CompanyOffer.jsx
@@ -0,0 +1,11 @@
+function Companyoffer() {
+  return (
+    <section>
+      <h2>Company Offer</h2>
+      <p>Package: ₹8 LPA</p>
+      <p>Location: Mumbai</p>
+    </section>
+  );
+}
+
+export default Companyoffer;
