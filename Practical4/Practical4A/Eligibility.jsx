5789a912cae5855c09faa7c2d2572f9afb8ddf21
bhavesh <bhavsalamvlu@gmail.com>
2026-07-19 19:04:56 +0530

    Add Eligibility component to display eligibility info

---
+++ Practical4/Practical4A/Eligibility.jsx
@@ -0,0 +1,11 @@
+function Eligibility() {
+  return (
+    <section>
+      <h2>Eligibility</h2>
+      <p>CGPA: 8.5</p>
+      <p>Status: Eligible</p>
+    </section>
+  );
+}
+
+export default Eligibility;
