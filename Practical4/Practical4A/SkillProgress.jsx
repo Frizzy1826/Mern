6b43b762852f5dd9a956561f20f1e6700e1ba468
bhavesh <bhavsalamvlu@gmail.com>
2026-07-19 19:06:23 +0530

    Add SkillProgress component to display skill levels

---
+++ Practical4/Practical4A/SkillProgress.jsx
@@ -0,0 +1,12 @@
+function Skillprogress() {
+  return (
+    <section>
+      <h2>Skill Progress</h2>
+      <p>React: 80%</p>
+      <p>JavaScript: 85%</p>
+      <p>Java: 75%</p>
+    </section>
+  );
+}
+
+export default Skillprogress;
